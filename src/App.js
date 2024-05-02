6828c4b9b583d418332c876b11411132bfa5118d
Sonjieun <sonjieun2661@gmail.com>
2024-05-02 15:53:42 +0900

    server-fix

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
+import React, { useEffect } from "react";
 
-function App() {
-  return (
-    <div className="App">
-       B팀의 시작
-    </div>
-  );
-}
+const App = () => {
+  useEffect(() => {
+    fetch("http://localhost:8080/api/posts")
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error("connect error");
+        }
+        return res.json();
+      })
+      .then((jsonData) => {
+        console.log(jsonData);
+      });
+  }, []);
+  return <div className="App">B팀의 시작</div>;
+};
 
 export default App;
